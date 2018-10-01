5f876d3be80ed55d8f68a1d990fb310895b247eb
Flint <flint@iMac1.local>
2018-10-01 09:55:51 -0400

    Finished version of flower project

--- assignment_three.py
+++ assignment_three.py
@@ -6,21 +6,39 @@ import turtle
 
 
 def get_length_hexagon():
+    """
+    asks user the length of the hexagon
+    :return: side length
+    """
     length = float(input("What is the side length of the hexagon?"))
     return length
 
 
 def get_petal_color():
+    """
+    asks user the petal color
+    :return: petal color
+    """
     petal_color = input("What color should the flower petals be?")
     return petal_color
 
 
 def get_center_color():
+    """
+    asks user center color
+    :return: center color
+    """
     center_color = input("What color should the center of the flower be?")
     return center_color
 
 
 def draw_hex(length, color):
+    """
+    draws a hexagon with side length and color chosen by user
+    :param length:
+    :param color:
+    :return:
+    """
     turtle.color(color)
     turtle.begin_fill()
     for x in range(6):
@@ -30,16 +48,22 @@ def draw_hex(length, color):
 
 
 def draw_flower():
+    """
+    Draws side six hexagons as petals around one hexagon in center all using user's color and side length
+    :return:
+    """
+    # This draws the center hexagon
     length_hex = get_length_hexagon()
     petal_color = get_petal_color()
     draw_hex(length_hex, get_center_color())
+    # Move to the correct location and draw a hexagon for petal
     turtle.penup()
     turtle.left(120)
     turtle.forward(length_hex)
     turtle.right(60)
     turtle.forward(length_hex)
     turtle.right(60)
-    turtle.pendown
+    turtle.pendown()
     draw_hex(length_hex, petal_color)
     turtle.penup()
     turtle.forward(length_hex)
@@ -90,4 +114,3 @@ main()
 
 
 turtle.exitonclick()
-
